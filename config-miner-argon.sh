a734bb734a0ed1efd278acdeed800de5d59e4fc5
Jason Schrader <whoabuddy@users.noreply.github.com>
2020-09-25 07:40:43 -0700

    add warning to argon script, fixes #30

--- config-miner-argon.sh
+++ config-miner-argon.sh
@@ -17,6 +17,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Add warning to beginning since this is from an older phase of testnet.
+printf '\n\e[1;31m%-6s\e[m\n' "NOTE: This script was created for a previous phase of the Blockstack Testnet (Argon), and is stored here for historic purposes."
+printf '\e[1;31m%-6s\e[m\n' "Please review the readme for the most updated version."
+read -rsn1 -p"Press any key to continue . . ."
+echo
+
 # Setup initial variables allowing for different
 # actions in the future, if needed.
 __action="${1:-}"
